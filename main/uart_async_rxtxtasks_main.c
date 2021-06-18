45d1afde671208d8726a8d0d3e216d80ca0d7a92
SL20032 <76879007+SL20032@users.noreply.github.com>
2021-06-18 16:28:12 +0200

    Update uart_async_rxtxtasks_main.c

--- main/uart_async_rxtxtasks_main.c
+++ main/uart_async_rxtxtasks_main.c
@@ -1,9 +1,3 @@
-/* UART asynchronous example, that uses separate RX and TX tasks
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
